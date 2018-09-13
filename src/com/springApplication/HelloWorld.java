26372b309b7123e650f9967c3c1993a28a8a26d9
Mihkel Tiganik <mtiganik@gmail.com>
2018-09-13 19:05:14 +0300

    Initial commit

---
+++ src/com/springApplication/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.springApplication;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public void getMessage(){
+        System.out.println("Message: " + message);
+    }
+}
